637c1ee19fc466716b9f39c8917baa15bc00a467
leo dechaumet <leo_dechaumet@protonmail.com>
2021-04-01 14:32:26 +0200

    visu

---
+++ ailoganalyzer/tools/visualisation.py
@@ -0,0 +1,13 @@
+from pylab import *
+import pandas as pd
+
+def seq_to_time_serie(seq_filename):
+
+    data = pd.read_csv(seq_filename)
+    data["sequence"] = data["sequence"].transform(lambda x : len(x.strip('][').split(', ')))
+    data["time"] = pd.to_datetime(data["time"],format = "%Y-%m-%d %H:%M:%S.%f")
+    return data
+
+def visualize_time_serie(time_serie):
+    time_serie.plot("time", "sequence")
+    show()
