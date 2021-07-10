764b32d0aa477aae1862d2a5b92b41d730c74404
leo dechaumet <leo_dechaumet@protonmail.com>
2021-07-10 10:47:17 +0200

    optimize slidding windows function for better space efficienty

--- ailoganalyzer/dataset/sample.py
+++ ailoganalyzer/dataset/sample.py
@@ -1,6 +1,41 @@
 import json
 import numpy as np
 from tqdm import tqdm
+from itertools import islice
+from torch.utils.data import Dataset
+from numpy import array, stack, vectorize, amax
+
+import torch
+
+
+class sliddingWindowDataset(Dataset):
+    def __init__(self, log_seq, labels, event2vec, windows_size, seq=True, quan=False, sem=False):
+        self.seq = seq
+        self.quan = quan
+        self.sem = sem
+        self.Sequentials = log_seq
+        self.event2vec = event2vec
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        log = dict()
+        seq = self.Sequentials[idx]
+        if self.seq:
+            log['Sequentials'] = torch.tensor(seq,
+                                              dtype=torch.float)
+        # TODO : quantitative
+        if self.quan:
+            log['Quantitatives'] = torch.tensor(self.Quantitatives[idx],
+                                                dtype=torch.float)
+        if self.sem:
+            sem = array([self.event2vec[e] for e in seq])
+            log['Semantics'] = torch.tensor(sem,
+                                            dtype=torch.float)
+        return log, self.labels[idx]
+
 
 def read_json(filename):
     with open(filename, 'r') as load_f:
@@ -8,9 +43,9 @@ def read_json(filename):
     return file_dict
 
 
-def trp(l, n):
+def trp(ls, n):
     """ Truncate or pad a list """
-    r = l[:n]
+    r = ls[:n]
     if len(r) < n:
         r.extend(list([0]) * (n - len(r)))
     return r
@@ -36,7 +71,26 @@ def down_sample(logs, labels, sample_ratio):
     return sample_logs, sample_labels
 
 
-def sliding_window(log_loader, sequences, num_classes, window_size, sample_ratio=1, semantic=True, system=""):
+def window_seq(seqs, n):
+    for seq in seqs:
+        if len(seq) > n:
+            seq = seq[:-1]
+        seq = map(int, seq)
+        it = iter(seq)
+        result = list(islice(it, n))
+        if len(result) == n:
+            yield array(result)
+        else:
+            yield result + list([0]) * (n - len(result))
+        for elem in it:
+            result = result[1:] + [elem]
+            yield array(result)
+
+
+#class slidding_window_dataset(Dataset)
+
+
+def sliding_window(log_loader, sequences, window_size, sample_ratio=1, semantic=True, system=""):
     '''
     dataset structure
         result_logs(dict):
@@ -45,58 +99,41 @@ def sliding_window(log_loader, sequences, num_classes, window_size, sample_ratio
             ...
         labels(list)
     '''
-
-    num_sessions = 0
+    print("slidding windows")
     result_logs = {}
     result_logs['Sequentials'] = []
     result_logs['Quantitatives'] = []
     result_logs['Semantics'] = []
+    print("get template to vec")
+    print("nb seq", len(sequences))
+
+    # Remove empty sequences
+    sequences = [seq for seq in sequences if len(seq) > 0]
+    print("nb seq", len(sequences))
+
+    event2vec = log_loader.template_to_vec_all(system)
+    print("got template to vec")
+    seq_it = window_seq(sequences, window_size)
+
     labels = []
+    for seq in tqdm(sequences):
+        if len(seq) > window_size:
+            labels.extend(seq[window_size:])
+        else:
+            labels.append(0)
+    print("start ee")
+    result_logs["Sequentials"] = stack(seq_it)
+    print(result_logs["Sequentials"].shape)
+    print(len(labels))
+    print("max ::", amax(result_logs["Sequentials"]))
+    print("max lab :: ", amax(labels))
+    dataset = sliddingWindowDataset(result_logs["Sequentials"], labels, event2vec, window_size, seq=False, quan=False, sem=True)
+    #vfunc = vectorize(event2vec.__getitem__)
+    #result_logs["Semantics"] = vfunc(result_logs["Sequentials"])
+    print("end ee")
 
-    for line in tqdm(sequences):
-        num_sessions += 1
-        line = [int(e) - 1 for e in line]
-        #sys.exit(0)
-        for i in range(len(line) - window_size):
-            Sequential_pattern = line[i:i + window_size]
-
-            #Quantitative_pattern = [0] * num_classes
-            #log_counter = Counter(Sequential_pattern)
-
-            #for key in log_counter:
-                #Quantitative_pattern[key] = log_counter[key]
-            if semantic:
-                Semantic_pattern = []
-                for event in Sequential_pattern:
-                    if event == -1:
-                        Semantic_pattern.append(np.array([-1] * 300))
-                    else:
-                        #start_time = time.perf_counter()
-                        Semantic_pattern.append(log_loader.template_to_vec(system,event))
-                        #end_time = time.perf_counter()
-                        #print("ex time:", end_time-start_time)
-
-            Sequential_pattern = np.array(Sequential_pattern)[:,
-                                                  np.newaxis]
-            Quantitative_pattern = []
-            Quantitative_pattern = np.array(
-                Quantitative_pattern)[:, np.newaxis]
-            result_logs['Sequentials'].append(Sequential_pattern)
-            result_logs['Quantitatives'].append(Quantitative_pattern)
-            if semantic:
-                result_logs['Semantics'].append(Semantic_pattern)
-
-            labels.append(line[i + window_size])
-    #print(labels)
-    if sample_ratio != 1:
-        result_logs, labels = down_sample(result_logs, labels, sample_ratio)
-
-    print('System {}, number of sessions {}'.format(system, num_sessions))
-    print('System {}, number of seqs {}'.format(system,
-                                              len(result_logs['Sequentials'])))
-
-    return result_logs, labels
+    return dataset
 
 
-def session_window(data_dir,num_classes, datatype, sample_ratio=1, semantic = False):
+def session_window(data_dir, num_classes, datatype, sample_ratio=1, semantic = False):
     raise NotImplementedError
